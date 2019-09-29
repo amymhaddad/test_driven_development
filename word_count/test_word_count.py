3f0071334613e22506b730b7f2b3895df6434902
Amy M Haddad <amy.m.haddad@gmail.com>
2019-09-29 12:02:01 -0400

    2 tests passing

--- word_count/test_word_count.py
+++ word_count/test_word_count.py
@@ -2,7 +2,25 @@
 from word_count import word_count
 
 def test_count_single_word():
-    #setup 
-    phrase = 'word'
+    assert word_count('word')
+    
 
-    assert word_count(phrase) == {'word': 1}
+def test_two_words_separated_by_single_space():
+    assert word_count('two words') == {'two':1, 'words': 1}
+    
+    # counter == count_each_phrase
+
+
+
+
+# def test_count_single_word():
+#     #setup 
+#     phrase = 'word'
+#     count_each_phrase = {'word': 1}
+
+#     #exercise 
+#     counter = word_count(phrase)
+
+#     #verify
+#     assert counter == count_each_phrase
+#     # assert word_count(phrase) == {'word': 1}
--- word_count/word_count.py
+++ word_count/word_count.py
@@ -1,3 +1,14 @@
 
 def word_count(phrase):
-    return {'word': 1}
+    counter = 0
+    word_count_dictionary = {}
+
+
+    for word in phrase.split(" "):
+        # import pdb; pdb.set_trace()
+        word_count_dictionary[word] = word_count_dictionary.get(word, 0) + 1
+    return word_count_dictionary
+
+print(word_count('two words'))
+
+
