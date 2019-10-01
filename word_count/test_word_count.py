cfb3bc0b111b0df1a7d7450c2a9c7429fe01c021
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-01 11:18:53 -0400

    5 tests passing and refactored program

--- word_count/test_word_count.py
+++ word_count/test_word_count.py
@@ -12,11 +12,10 @@ def test_repeated_words_separated_by_single_space():
     assert word_count('house house dog') == {'house': 2, 'dog':1}
 
 def test_words_with_capital_letters():
-    assert word_count('My name is Jill and my home is in Boston') == {'my': 2, 'name': 1, 'is': 2, 'Jill': 1, 'and': 1, 'home': 1, 'in': 1, 'Boston': 1}
-    # counter == count_each_phrase
+    assert word_count('My name is Jill and my home is in Boston') == {'my': 2, 'name': 1, 'is': 2, 'jill': 1, 'and': 1, 'home': 1, 'in': 1, 'boston': 1}
 
 def test_phrase_with_punctuation():
-    assert word_count('My name is Jill and my home is in Boston!') == {'my': 2, 'name': 1, 'is': 2, 'Jill': 1, 'and': 1, 'home': 1, 'in': 1, 'Boston': 1}
+    assert word_count('My name is Jill and my home is in Boston!') == {'my': 2, 'name': 1, 'is': 2, 'jill': 1, 'and': 1, 'home': 1, 'in': 1, 'boston': 1}
 
 
 
--- word_count/word_count.py
+++ word_count/word_count.py
@@ -1,37 +1,28 @@
 from string import punctuation as punctuation_marks
 
-# phrase = 'My! name is Jill and my home is in Boston!'
+phrase = 'My name is Jill and my home is in Boston!'
 
 def word_count(phrase):
 
-    phrase = remove_puncutation(phrase)
+    phrase = validate_phrase(phrase)
 
     counter = 0
     word_count_dictionary = {}
 
-
-    for i, word in enumerate(phrase.split(" ")):
-        if i == 0: 
-            word = word.lower()
-
-        
+    for word in phrase.split(" "):
         word_count_dictionary[word] = word_count_dictionary.get(word, 0) + 1
     return word_count_dictionary
 
 
-
-# {'my': 2, 'name': 1, 'is': 2, 'Jill': 1, 'and': 1, 'home': 1, 'in': 1, 'boston': 1}
-
-
-def remove_puncutation(phrase):
+def validate_phrase(phrase):
+    """Remove punctuation and lowercase all words"""
 
     updated_phrase = ''
-    for words in phrase:
-        for letter in words:
-            if letter in punctuation_marks:
-                updated_phrase += words.strip(punctuation_marks)
-            else:
-                updated_phrase += words
+    for words in phrase.lower():
+        if words in punctuation_marks:
+            updated_phrase += words.strip(punctuation_marks)
+        else:
+            updated_phrase += words
     return updated_phrase
 
-# print(word_count(phrase))
+print(word_count(phrase))
