0e61ca0b0f40c3f5a11a7f7abeb7cd3e028f8da3
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-04 10:45:40 -0400

    Final changes for PR

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,17 +1,20 @@
-
-letter = 'I'
-
 scrabble_score = {
-    'A, E, I, O, U, L, N, R, S, T' : 1,
-
+    "A, E, I, O, U, L, N, R, S, T": 1,
+    "D, G": 2,
+    "B, C, M, P": 3,
+    "F, H, V, W, Y ": 4,
+    "K": 5,
+    "J,X": 8,
+    "Q,Z": 10,
 }
 
 
 def scrabble(word):
+    """Calculate the scrabble score for a given word"""
 
-
-    return 1
-
-for letters, score in scrabble_score.items():
-    if letter in letters:
-        print(score)
+    total_score = 0
+    for letters, score in scrabble_score.items():
+        for letter in word.lower():
+            if letter in letters.lower():
+                total_score += score
+    return total_score
--- scrabble/test_scrabble.py
+++ scrabble/test_scrabble.py
@@ -1,7 +1,21 @@
 from scrabble import scrabble
 
-def test_one_letter_word():
-    assert scrabble('I') == 1
 
-# def test_two_letter_word():
-#     assert scrabble('of') == 5
+def test_no_word():
+    assert scrabble("") == 0
+
+
+def test_one_letter_word_uppercase():
+    assert scrabble("I") == 1
+
+
+def test_two_letter_word_lowercase():
+    assert scrabble("of") == 5
+
+
+def test_captialized_word():
+    assert scrabble("Amy") == 8
+
+
+def test_long_word():
+    assert scrabble("cabbage") == 14
