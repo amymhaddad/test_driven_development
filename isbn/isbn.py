2f3bb124a15229d3cdb5a3455defd81a9725c925
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-08 11:13:11 -0400

    Algorithm works

--- isbn/isbn.py
+++ isbn/isbn.py
@@ -3,24 +3,37 @@
 number = '3598215088'
 
 def isbn(number):
+    total = 0
+    index = [digit for digit in reversed(range(11))]
+    numbers = [int(num) for num in number]
+
+    #validity check to be exceptions 
     if number == '' or len(number) < 10:
         return False
+    else:
+        for num, i in zip(numbers, index):
+            print(num, i)
+            total += num * i 
+       
+        return bool(total % 11 == 0)
 
+    
 
+print(isbn(number))
     
     
     
 
-def validate_number(number):
+# def validate_number(number):
     
-    return [int(num) for num in number]
+#     return [int(num) for num in number]
 
-numbers = validate_number(number)
+# numbers = validate_number(number)
 
-index = [digit for digit in reversed(range(11))]
+# index = [digit for digit in reversed(range(11))]
 
 
-total = 0
-for n, i in zip(numbers, index):
-    total += n*i
-print(total % 11)
+# total = 0
+# for n, i in zip(numbers, index):
+#     total += n*i
+# print(total % 11)
