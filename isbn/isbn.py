024b62d7bc52fd5973fd2d380c2a168c9a439e78
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-09 11:14:07 -0400

    Final version of isbn program and test for PR

--- isbn/isbn.py
+++ isbn/isbn.py
@@ -1,28 +1,25 @@
 from string import digits
 
-number = '359821507X'
-# number = ''
-
-
 
 def isbn(number):
+    """Determine if a given number is a valid isbn number"""
+
     total = 0
     index = [digit for digit in reversed(range(11))]
-    numbers = [int(num) for num in number if num in digits]
+    numbers = standardize_isbn(number)
 
-    if not valid_number(number):
+    if len(numbers) < 10:
         return False
 
-    if number[-1] == 'X':
-            total += 10
     for num, i in zip(numbers, index):
-        total += num * i 
+        total += num * i
     return bool(total % 11 == 0)
-  
 
 
-def valid_number(number):
-    return bool(len(number) == 10 )
-    
- 
-print(isbn(number))   
+def standardize_isbn(number):
+    """Convert given string to integers"""
+    numbers = [int(num) for num in number if num in digits]
+
+    if number[-1] == "X":
+        numbers.append(10)
+    return numbers
--- isbn/test_isbn.py
+++ isbn/test_isbn.py
@@ -1,21 +1,26 @@
 from isbn import isbn
 
+
 def test_empty_string_of_numbers():
-    number = ''
+    number = ""
     assert isbn(number) == False
 
-def test_short_number():
-    number = '35982150'
+
+def test_isbn_not_correct_length():
+    number = "35982150"
     assert isbn(number) == False
 
-def test_isbn_without_hyphens():
-    number = '3598215088'
-    assert isbn(number) == True 
 
-def test_isbn_ends_with_x():
-    number = '359821507X'
+def test_isbn_ends_with_X():
+    number = "359821507X"
     assert isbn(number) == True
 
 
-#check for dashses or not dashes
-#check ends with number
+def test_isbn_without_hyphens():
+    number = "3598215088"
+    assert isbn(number) == True
+
+
+def test_isbn_with_hypens():
+    number = "3-598-21507-X"
+    assert isbn(number) == True
