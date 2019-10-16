8618b27715ebc1c7df6a1447df0d206e1d7e7a11
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-16 10:39:07 -0400

    Final changes to robot name for PR review

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -2,20 +2,13 @@ import random
 from string import ascii_uppercase as upper
 from string import digits as digit
 
-class Robot():
 
+class Robot:
     def __init__(self):
         self.name = self.create_random_name()
-    
+
     def create_random_name(self):
         return "".join(random.sample(upper, 2) + random.sample(digit, 3))
 
     def reset(self):
         return self.create_random_name()
-
-
-
-# r = Robot()
-
-# print(r.create_random_name())
-# print(r.reset())
--- robot_name/test_robot_name.py
+++ robot_name/test_robot_name.py
@@ -1,10 +1,17 @@
+from robot_name import Robot
 
-from robot_name import Robot 
 
-def test_robot_has_name():
-    robot = Robot()
-    assert robot.name != robot.create_random_name()
+def test_robots_have_different_names():
+    robot1 = Robot()
+    robot2 = Robot()
+    assert robot1.name != robot2.name
+
 
 def test_reset_name_to_different_name():
     robot = Robot()
     assert robot.reset() != robot.name
+
+
+def test_length_robot_name():
+    robot = Robot()
+    assert len(robot.name) == 5
