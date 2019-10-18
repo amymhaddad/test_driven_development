0c4d214940f2e7e63e5899ee8382501f60585661
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-18 11:23:59 -0400

    Final changes for PR

--- bob/bob.py
+++ bob/bob.py
@@ -1,10 +1,14 @@
-
-# phrase = "WHY ARE YOU ALWAYS LATE?"
-
 def speak(phrase):
     if phrase.isupper():
-        return "Calm down, I know what I'm doing" if phrase.endswith("?") else 'Whoa, chill out!'
+        return (
+            "Calm down, I know what I'm doing"
+            if phrase.endswith("?")
+            else "Whoa, chill out!"
+        )
 
     elif phrase.endswith("?"):
-        return 'Sure'
+        return "Sure"
 
+    elif phrase.endswith("."):
+        return "Fine. Be that way!"
+    return "Whatever."
--- bob/test_bob.py
+++ bob/test_bob.py
@@ -1,14 +1,26 @@
+from bob import speak
 
-from bob import speak 
+
+def test_say_nothing_to_bob():
+    phrase = ""
+
+    assert speak(phrase) == "Whatever."
+
+
+def test_say_statement_to_bob():
+    phrase = "It's a lovely day outside."
+
+    assert speak(phrase) == "Fine. Be that way!"
+
 
 def test_ask_bob_question():
     phrase = "Are you going to dinner tonight?"
 
-    assert speak(phrase) == 'Sure'
+    assert speak(phrase) == "Sure"
 
 
 def test_yell_statement_at_bob():
-    phrase = "YOU CAME HOME AFTER MIDNIGHT!"
+    phrase = "THE HOUSE IS A MESS!"
 
     assert speak(phrase) == "Whoa, chill out!"
 
