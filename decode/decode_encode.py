c53f66e006d52dd2b9ce9f9089f6177c306aeccf
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-23 10:59:16 -0400

    Got algorithm working for decode function to return correct string when given multiple letters (same or differnet)

--- decode/decode_encode.py
+++ decode/decode_encode.py
@@ -3,27 +3,31 @@
 def encode(string):
 
     duplicate_letter = ''
-    letter_count = ''
+    letter_count = []
     count = 0
 
-    for i, letter in enumerate(string):
+    for i, letter in enumerate(string):        
         # import pdb; pdb.set_trace()
         
         if not duplicate_letter:
-
             duplicate_letter += letter
             count += 1
 
         elif letter in duplicate_letter:
             duplicate_letter += letter
             count += 1
+            
+        else:     
+            letter_count.append(str(count))
+            letter_count.append(duplicate_letter[0])
+            letter_count.append(letter)
         
-        # elif letter not in duplicate_letter:
-        #     letter_count += 
-        return duplicate_letter
+        letter = duplicate_letter 
+            
+    return "".join(letter_count)
        
 
     # return "3W"
 
-print(encode('WWW'))
+print(encode('WWWBWWW'))
 
--- decode/test_decode_encode.py
+++ decode/test_decode_encode.py
@@ -2,3 +2,9 @@ from decode_encode import encode
 
 def test_encode_string_same_letter_all_caps():
     return encode('WWW') == "3W"
+
+def test_encode_string_multiple_letters_all_caps():
+    return encode('WWWBBB') == "3W3B"
+
+def test_encode_string_multiple_letters_with_single_letter_all_caps():
+    return encode('WWWB') == "3WB"
