a72c68ff9a0769c3d9b3f76dc7eafa09bad06ca2
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-24 11:18:24 -0400

    Completed encoded function tests and program; on decode function. Part of the algorithm works

--- decode/decode_encode.py
+++ decode/decode_encode.py
@@ -4,29 +4,38 @@ def encode(string):
 
     duplicate_letter = ''
     letter_count = []
-    # count = 0
 
     for i, letter in enumerate(string):        
         
         if duplicate_letter == '' or letter in duplicate_letter:
-            # import pdb; pdb.set_trace()
             duplicate_letter += letter
             
-            
-        elif letter not in duplicate_letter:
-            letter_count.append(len(duplicate_letter))
+        else:
+            letter_count.append(str(len(duplicate_letter)))
             letter_count.append(duplicate_letter[0])
             duplicate_letter = letter
             
     letter_count.append(letter)   
             
-                
-               
-        
+    return "".join(letter_count)
+  
+# print(encode('WWB'))
 
-    return letter_count
-       
+from string import digits
+
+def decode(string):
+    decoded_string = []
+    
+    for letter in string:
+        if letter in digits:
+            number = int(letter)
+
+    decoded_string.append(number * letter)
+
+    return "".join(decoded_string)
 
 
-print(encode('WWB'))
+print(decode("3W3B"))
+
+#  return decode("3W") == "WWW"
 
--- decode/test_decode_encode.py
+++ decode/test_decode_encode.py
@@ -1,10 +1,22 @@
-from decode_encode import encode 
+from decode_encode import encode, decode 
 
-def test_encode_string_same_letter_all_caps():
+def test_encode_string_same_letter():
     return encode('WWW') == "3W"
 
-def test_encode_string_multiple_letters_all_caps():
+def test_encode_string_multiple_different_letters():
     return encode('WWWBBB') == "3W3B"
 
-def test_encode_string_multiple_letters_with_single_letter_all_caps():
+def test_encode_string_multiple_same_letters_with_single_unique_letter():
     return encode('WWWB') == "3WB"
+
+def test_endcode_string_multiple_same_letters_and_muliple_single_uniqu_letters():
+    return encode('WWWBWWWCBB') == "3WB3WC2B"
+
+def test_encode_long_diverse_string():
+    return encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") == "12WB12W3B24WB"
+
+def test_decode_string_same_letter():
+    return decode("3W") == "WWW"
+
+def test_decode_string_multiple_different_letters():
+    return decode("3W3B") == "WWWBBB"
