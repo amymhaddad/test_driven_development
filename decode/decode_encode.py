15bfc84f120bc7201a1bc73806b011f5068bfece
Amy M Haddad <amy.m.haddad@gmail.com>
2019-10-28 11:30:56 -0400

    WIP - creating a main() function for encode and decode

--- decode/decode_encode.py
+++ decode/decode_encode.py
@@ -1,3 +1,14 @@
+from string import digits
+
+
+def encode_decode(string):
+    if string == "":
+        return ""
+    
+    if encode(string):
+        return encode(string)
+    return decode(string)
+
 
 
 def encode(string):
@@ -11,31 +22,14 @@ def encode(string):
             duplicate_letter += letter
             
         else:
-            letter_count.append(str(len(duplicate_letter)))
-            letter_count.append(duplicate_letter[0])
+            letter_count += str(len(duplicate_letter)) + duplicate_letter[0]
             duplicate_letter = letter
             
     letter_count.append(letter)   
             
     return "".join(letter_count)
   
-# print(encode('WWB'))
-
-from string import digits
 
 def decode(string):
-    decoded_string = []
-    
-    for letter in string:
-        if letter in digits:
-            number = int(letter)
-
-    decoded_string.append(number * letter)
-
-    return "".join(decoded_string)
-
-
-print(decode("3W3B"))
-
-#  return decode("3W") == "WWW"
+    return "".join([int(letter) * letter   for letter in string if letter in digits])
 
