84cce5942c1023bc2fec2a1bb4ae535bc8dfe4aa
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-08 11:01:19 -0500

    All tests pass for iteration 1 of program with set()

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,13 +1,18 @@
 
+
+
+
+def validate_string(string):
+    return "".join([letter for letter in string.lower() if letter.isalpha()])
+
 def isogram(string):
+
+    string = validate_string(string)
 
     unique_letters = set()
     for letter in string:
         unique_letters.add(letter)
+    
     if len(unique_letters) == len(string):
         return True
     return False
-            
-
-
-print(isogram('catt'))
--- isogram/test_isogram.py
+++ isogram/test_isogram.py
@@ -1,4 +1,19 @@
 from isogram import isogram
 
-def test_word_without_repeating_letters():
+def test_short_word_is_isogram():
     assert isogram("cat") == True
+
+def test_long_word_is_isogram():
+    assert isogram("lumberjacks") == True
+
+def test_long_word_is_not_isogram():
+    assert isogram("isograms") == False
+
+def test_phrase_with_spaces_is_isogram():
+    assert isogram("if only") == True
+
+def test_phrase_with_hyphens():
+    assert isogram("six-year-old") == True
+
+def test_phrase_with_mixed_case():
+    assert isogram("Emily Jung") == True
