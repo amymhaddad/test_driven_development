d89f7f46d6709bc74e7ddff764468cabdfcce78d
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-09 10:59:07 -0500

    Two versions of isogram for PR review

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,36 +1,27 @@
-
-
-
-
 def validate_string(string):
     return "".join([letter for letter in string.lower() if letter.isalpha()])
 
-# def isogram(string):
 
-#     string = validate_string(string)
+# Option 1 - use set()
+def isogram(string):
 
-#     unique_letters = set()
-#     for letter in string:
-#         unique_letters.add(letter)
-    
-#     if len(unique_letters) == len(string):
-#         return True
-#     return False
+    string = validate_string(string)
+
+    unique_letters = set()
+    for letter in string:
+        unique_letters.add(letter)
+
+    if len(unique_letters) == len(string):
+        return True
+    return False
 
 
-word = "batt"
-
-
-def isogram(word):
-    # string = validate_string(string)
+# Option 2 = use for loop
+def isogram(string):
+    string = validate_string(string)
     count = 0
-    for i, outer_letter in enumerate(word):
-        for inner_letter in word[i+1:]:
-            # import pdb; pdb.set_trace()
+    for i, outer_letter in enumerate(string):
+        for inner_letter in string[i + 1 :]:
             if outer_letter == inner_letter:
                 count += 1
-    return count
-            # if letters == letter:
-            #     return False
-
-print(isogram(word))
+    return bool(count < 1)
