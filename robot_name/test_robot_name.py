dcc6afc2f9443e21980483e5add86b6981f9631f
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-09 11:17:52 -0500

    WIP - updating tests from PR review feedback

--- robot_name/test_robot_name.py
+++ robot_name/test_robot_name.py
@@ -1,5 +1,11 @@
 from robot_name import Robot
 
+import pytest
+
+@pytest.fixture
+def robot():
+     return Robot()
+
 
 def test_robots_have_different_names():
     robot1 = Robot()
@@ -7,9 +13,10 @@ def test_robots_have_different_names():
     assert robot1.name != robot2.name
 
 
-def test_reset_name_to_different_name():
-    robot = Robot()
-    assert robot.reset() != robot.name
+def test_reset_name_to_different_name(robot):
+    original_name = robot.name
+    robot.reset()
+    assert original_name != robot.name
 
 
 def test_length_robot_name():
