94a9ecdc45c35f8eeb5253dd218582a718584e05
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-13 11:23:23 -0500

    Algorithm works; 2 tests pass

--- rotational_cipher/rotational.py
+++ rotational_cipher/rotational.py
@@ -2,11 +2,34 @@
 
 from string import ascii_lowercase as letters
 
-string = 'm'
-rotate = 13
+
+def rotate(string, key):
+
+    if key == 0:
+        return string
+    new = []
+    for i, outer_letter in enumerate(letters):
+        for inner_letter in string: 
+            if inner_letter == outer_letter:
+                new.append(letters[i+key%26])
+    new.reverse()
+    return "".join(new)
+
+# print(rotate('omg', 5))
+
+# string = 'omg'
+# key = 5
+# new = []
+# for i, outer_letter in enumerate(letters):
+#     for inner_letter in string: 
+#        if inner_letter == outer_letter:
+#            new.append(letters[i+key%26])
+# new.reverse()
+# print(new)
 
 
-for i, letter in enumerate(letters):
-    if letter == string:
-        #add an index of 13 to string
-        print(letters[i%26])
+
+
+    
+
+
--- rotational_cipher/test_rotational.py
+++ rotational_cipher/test_rotational.py
@@ -1,4 +1,7 @@
+from rotational import rotate
 
+def test_rotate_by_0():
+    assert rotate('a', 0) == 'a'
 
-def test_rotate_a_by_0(self):
-        self.assertEqual(rotational_cipher.rotate('a', 0), 'a')
+def test_rotate_by_5():
+    assert rotate('omg', 5) == 'trl'
