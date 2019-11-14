0bc55793a6322ae3a452b47a33b32e196a6a2ea3
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-14 11:21:49 -0500

    3 tests passing

--- rotational_cipher/rotational.py
+++ rotational_cipher/rotational.py
@@ -10,10 +10,15 @@ def rotate(string, key):
         for outer_letter in string:
             for i, inner_letter in enumerate(letters):
                 if inner_letter == outer_letter.lower():
+                    # import pdb; pdb.set_trace()
                     if outer_letter.isupper():
-                        new += letters[i+key%26].capitalize()
+                        new += letters[i+key%26].capitalize() 
                     else:
                         new += letters[i+key%26]
         return new
 
-# print(rotate(string, key)
+# print(rotate('The quick brown fox jumps over the lazy dog', 13))
+
+#  'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.'
+
+#Create new function for phrases; use .split(" ") and a double for loop 
