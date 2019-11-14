2bffce27fd0c099dcc783d2649839e317e8e84e4
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-14 11:10:29 -0500

    Program accounts for capitalized letters

--- rotational_cipher/rotational.py
+++ rotational_cipher/rotational.py
@@ -1,35 +1,5 @@
-# 
 
 from string import ascii_lowercase as letters
-
-
-# def rotate(string, key):
-
-#     if key == 0:
-#         return string
-#     new = []
-#     for i, outer_letter in enumerate(letters):
-#         for inner_letter in string: 
-#             if inner_letter == outer_letter:
-#                 new.append(letters[i+key%26])
-#     new.reverse()
-#     return "".join(new)
-
-
-# print(rotate('abc', 2))
-
-# assert rotate('cool', 26) == 'cool'
-
-
-string = 'omg'
-
-key = 5
-
-# new = ''
-# for i, outer_letter in enumerate(letters):
-#     for ii, inner_letter in enumerate(string):
-#         if inner_letter == outer_letter:
-#             print(letters[i+key])
 
 def rotate(string, key):
     if key == 0:
@@ -37,11 +7,13 @@ def rotate(string, key):
 
     else:
         new = ''
-        for i, outer_letter in enumerate(string):
-            # import pdb; pdb.set_trace()
-            for ii, inner_letter in enumerate(letters):
-                if inner_letter == outer_letter:
-                    new += letters[ii+key]
+        for outer_letter in string:
+            for i, inner_letter in enumerate(letters):
+                if inner_letter == outer_letter.lower():
+                    if outer_letter.isupper():
+                        new += letters[i+key%26].capitalize()
+                    else:
+                        new += letters[i+key%26]
         return new
 
-print(rotate(string, key))
+# print(rotate(string, key)
--- rotational_cipher/test_rotational.py
+++ rotational_cipher/test_rotational.py
@@ -7,5 +7,7 @@ def test_rotate_by_5():
     assert rotate('omg', 5) == 'trl'
 
 def test_rotate_by_26():
-    assert rotate('cool', 26) == 'cool'
+    assert rotate('Cool', 26) == 'Cool'
 
+# def test_rotate_by_13:
+#     assert rotate('The quick brown fox jumps over the lazy dog', 13 == 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.')
