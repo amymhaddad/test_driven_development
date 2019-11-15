b674a607f22feea3af11b81376a334ab02f0b3c2
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-15 11:20:49 -0500

    Updated algorithm

--- rotational_cipher/rotational.py
+++ rotational_cipher/rotational.py
@@ -1,6 +1,7 @@
 
 from string import ascii_lowercase as letters
 
+
 def rotate(string, key):
     if key == 0:
         return string
@@ -9,16 +10,13 @@ def rotate(string, key):
         new = ''
         for outer_letter in string:
             for i, inner_letter in enumerate(letters):
+                shift_value = (i + key) %26
                 if inner_letter == outer_letter.lower():
-                    # import pdb; pdb.set_trace()
                     if outer_letter.isupper():
-                        new += letters[i+key%26].capitalize() 
+                        new += letters[shift_value].capitalize()
                     else:
-                        new += letters[i+key%26]
+                        new += letters[shift_value]        
         return new
 
-# print(rotate('The quick brown fox jumps over the lazy dog', 13))
+# print(rotate('The', 13))
 
-#  'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.'
-
-#Create new function for phrases; use .split(" ") and a double for loop 
--- rotational_cipher/test_rotational.py
+++ rotational_cipher/test_rotational.py
@@ -9,5 +9,5 @@ def test_rotate_by_5():
 def test_rotate_by_26():
     assert rotate('Cool', 26) == 'Cool'
 
-# def test_rotate_by_13():
-#     assert rotate('The quick brown fox jumps over the lazy dog', 13) == 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.'
+def test_rotate_by_13():
+    assert rotate('The quick brown fox jumps over the lazy dog.', 13) == 'Gur dhvpx oebja sbk whzcf bire gur ynml qbts.'
