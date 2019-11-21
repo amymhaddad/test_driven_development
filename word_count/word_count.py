2c0fd3e62b45c8b1cc7d375bd0325ede0c3ca96d
Amy M Haddad <amy.m.haddad@gmail.com>
2019-11-21 11:17:57 -0500

    Refactor word_count algorithm based off of PR review

--- word_count/word_count.py
+++ word_count/word_count.py
@@ -7,6 +7,8 @@ def word_count(phrase):
     phrase = validate_phrase(phrase)
     word_count_dictionary = {}
 
+
+
     if len(phrase) < 1:
         return 0
 
@@ -20,15 +22,26 @@ def word_count(phrase):
 def validate_phrase(phrase):
     """Remove punctuation and digits; lowercase all words"""
 
-    updated_phrase = ""
-    for words in phrase.lower():
-        if words in punctuation:
-            if words == "-":
-                words.replace("-", "")
-            updated_phrase += words.strip(punctuation)
-        elif words in digits:
-            continue
-        else:
-            updated_phrase += words
+    updated_phrase = []
+    for words in phrase.lower().split(" "):
 
+        if words.isalpha():
+            updated_phrase.append(words)
+        elif words.isdigit():
+            continue
+
+    
     return updated_phrase
+
+        # if words in punctuation:
+        #     print(words)
+        #     updated_phrase += words.strip(punctuation) + " "
+        # elif words in digits:
+        #     continue
+        # else:
+        #     updated_phrase += words + " "
+    # return updated_phrase
+
+phrase = "I am 12 years old."
+
+print(validate_phrase(phrase))
